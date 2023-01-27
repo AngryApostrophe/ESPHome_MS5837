887b0f817c2d20873c5080d2ee2693db31bec533
AngryApostrophe <89547888+AngryApostrophe@users.noreply.github.com>
2023-01-26 20:43:24 -0800

    Fix encoding
    Fixed utf-8 encoding issue

--- esphome/MS5837_Component.h
+++ esphome/MS5837_Component.h
@@ -36,7 +36,7 @@
 #define MS5837_UNITS_TEMP_R        3
 
 
-class MS5837_Component : public PollingComponent, public Sensor, public CustomAPIDevice 
+class MS5837_Component : public PollingComponent, public Sensor, public CustomAPIDevice
 {
 public:
 
@@ -72,7 +72,7 @@ public:
 			Wire.beginTransmission(MS5837_ADDR);
 			Wire.write(MS5837_RESET);
 			err = Wire.endTransmission();
-			if (err != 0) 
+			if (err != 0)
 			{
 				ESP_LOGE("custom", "MS5837:  Comms error resetting device.  Error no %d.", err);
 				return;
@@ -82,12 +82,12 @@ public:
 			delay(10);
 
 		// Read calibration values and CRC
-			for ( uint8_t i = 0 ; i < 7 ; i++ ) 
+			for ( uint8_t i = 0 ; i < 7 ; i++ )
 			{
 				Wire.beginTransmission(MS5837_ADDR);
 				Wire.write(MS5837_PROM_READ+i*2);
 				err = Wire.endTransmission();
-				if (err != 0) 
+				if (err != 0)
 				{
 					ESP_LOGE("custom", "MS5837:  Comms error reading calibration values  Error no %d.", err);
 					return;
@@ -107,7 +107,7 @@ public:
 			uint8_t crcRead = C[0] >> 12;
 			uint8_t crcCalculated = crc4(C);
 
-			if ( crcCalculated != crcRead ) 
+			if ( crcCalculated != crcRead )
 			{
 				ESP_LOGE("custom", "MS5837:  Initialization failed.  CRC error.  Error no %d.", err);
 				return;
@@ -129,7 +129,7 @@ public:
 		bInitialized = true;
 		ESP_LOGI("custom", "MS5837:  MS5837 initialized.");
 	}
-    
+
 	void update()
 	{
 		uint32_t err;
@@ -148,8 +148,8 @@ public:
 		// Request D1 (raw pressure)
 			Wire.beginTransmission(MS5837_ADDR);
 			Wire.write(MS5837_CONVERT_D1_8192);
-			err = Wire.endTransmission(); 
-			if (err != 0) 
+			err = Wire.endTransmission();
+			if (err != 0)
 			{
 				ESP_LOGE("custom", "MS5837:  Comms error requesting D1.  Error no %d.", err);
 				InvalidateSensors();
@@ -161,8 +161,8 @@ public:
 		//Read the response
 			Wire.beginTransmission(MS5837_ADDR);
 			Wire.write(MS5837_ADC_READ);
-			err = Wire.endTransmission(); 
-			if (err != 0) 
+			err = Wire.endTransmission();
+			if (err != 0)
 			{
 				ESP_LOGE("custom", "MS5837:  Comms error requesting D1 Read.  Error no %d.", err);
 				InvalidateSensors();
@@ -170,7 +170,7 @@ public:
 			}
 
 			err = Wire.requestFrom(MS5837_ADDR,3);
-			if (err != 3) 
+			if (err != 3)
 			{
 				ESP_LOGE("custom", "MS5837:  Comms error reading D1 reply.  Received invalid response.");
 				InvalidateSensors();
@@ -185,8 +185,8 @@ public:
 		// Request D2 (raw temperature)
 			Wire.beginTransmission(MS5837_ADDR);
 			Wire.write(MS5837_CONVERT_D2_8192);
-			err = Wire.endTransmission(); 
-			if (err != 0) 
+			err = Wire.endTransmission();
+			if (err != 0)
 			{
 				ESP_LOGE("custom", "MS5837:  Comms error requesting D2.  Error no %d.", err);
 				InvalidateSensors();
@@ -198,8 +198,8 @@ public:
 		//Read the response
 			Wire.beginTransmission(MS5837_ADDR);
 			Wire.write(MS5837_ADC_READ);
-			err = Wire.endTransmission(); 
-			if (err != 0) 
+			err = Wire.endTransmission();
+			if (err != 0)
 			{
 				ESP_LOGE("custom", "MS5837:  Comms error requesting D2 Read.  Error no %d.", err);
 				InvalidateSensors();
@@ -207,7 +207,7 @@ public:
 			}
 
 			err = Wire.requestFrom(MS5837_ADDR,3);
-			if (err != 3) 
+			if (err != 3)
 			{
 				ESP_LOGE("custom", "MS5837:  Comms error reading D2 reply.  Received invalid response.");
 				InvalidateSensors();
@@ -228,12 +228,12 @@ public:
 
 			if (bPCalcMode == MS5837_MODE_ALTITUDE)
 				altitude_sensor->publish_state(ConvertAlt(Altitude()));
-			else if (bPCalcMode == MS5837_MODE_DEPTH)    
+			else if (bPCalcMode == MS5837_MODE_DEPTH)
 				altitude_sensor->publish_state(ConvertAlt(Depth()));
 	}
-    
+
 	//Run the calculations from the raw temp/press reported by the sensor
-	void calculate() 
+	void calculate()
 	{
 		// Given C1-C6 and D1, D2, calculated TEMP and P
 		// Do conversion first and then second order temp compensation
@@ -249,13 +249,13 @@ public:
 
 		// Terms called
 			dT = D2_temp-uint32_t(C[5])*256l;
-			if (  _model == MS5837_VERSION_02BA01 || _model == MS5837_VERSION_02BA21 || _model == MS5837_VERSION_02BA06) 
+			if (  _model == MS5837_VERSION_02BA01 || _model == MS5837_VERSION_02BA21 || _model == MS5837_VERSION_02BA06)
 			{
 				SENS = int64_t(C[1])*65536l+(int64_t(C[3])*dT)/128l;
 				OFF = int64_t(C[2])*131072l+(int64_t(C[4])*dT)/64l;
 				P = (D1_pres*SENS/(2097152l)-OFF)/(32768l);
-			} 
-			else 
+			}
+			else
 			{
 				SENS = int64_t(C[1])*32768l+(int64_t(C[3])*dT)/256l;
 				OFF = int64_t(C[2])*65536l+(int64_t(C[4])*dT)/128l;
@@ -266,30 +266,30 @@ public:
 			TEMP = 2000l+int64_t(dT)*C[6]/8388608LL;
 
 		//Second order compensation
-			if ( _model == MS5837_VERSION_02BA01 || _model == MS5837_VERSION_02BA21 || _model == MS5837_VERSION_02BA06) 
+			if ( _model == MS5837_VERSION_02BA01 || _model == MS5837_VERSION_02BA21 || _model == MS5837_VERSION_02BA06)
 			{
 				if((TEMP/100)<20) //Low temp
-				{         
+				{
 					Ti = (11*int64_t(dT)*int64_t(dT))/(34359738368LL);
 					OFFi = (31*(TEMP-2000)*(TEMP-2000))/8;
 					SENSi = (63*(TEMP-2000)*(TEMP-2000))/32;
 				}
-			} 
-			else 
+			}
+			else
 			{
 				if((TEMP/100)<20) //Low temp
-				{         
+				{
 					Ti = (3*int64_t(dT)*int64_t(dT))/(8589934592LL);
 					OFFi = (3*(TEMP-2000)*(TEMP-2000))/2;
 					SENSi = (5*(TEMP-2000)*(TEMP-2000))/8;
 					if((TEMP/100)<-15)  //Very low temp
-					{   
+					{
 						OFFi = OFFi+7*(TEMP+1500l)*(TEMP+1500l);
 						SENSi = SENSi+4*(TEMP+1500l)*(TEMP+1500l);
 					}
 				}
 				else if((TEMP/100)>=20) //High temp
-				{    
+				{
 					Ti = 2*(dT*dT)/(137438953472LL);
 					OFFi = (1*(TEMP-2000)*(TEMP-2000))/16;
 					SENSi = 0;
@@ -297,16 +297,16 @@ public:
 			}
 
 		//Calculate pressure and temp second order
-			OFF2 = OFF-OFFi; 
+			OFF2 = OFF-OFFi;
 			SENS2 = SENS-SENSi;
 
 			TEMP = (TEMP-Ti);
 
-			if ( _model == MS5837_VERSION_02BA01 || _model == MS5837_VERSION_02BA21 || _model == MS5837_VERSION_02BA06) 
+			if ( _model == MS5837_VERSION_02BA01 || _model == MS5837_VERSION_02BA21 || _model == MS5837_VERSION_02BA06)
 			{
 				P = (((D1_pres*SENS2)/2097152l-OFF2)/32768l);
-			} 
-			else 
+			}
+			else
 			{
 				P = (((D1_pres*SENS2)/2097152l-OFF2)/8192l);
 			}
@@ -329,7 +329,7 @@ public:
 
 			ESP_LOGI("custom", "MS5837:  Atmospheric Pressure updated to %0.2f hPa", num_float/100.0f);
 		}
-    
+
 	//Allow user to change target units from ESPHome device yaml
 		void SetUnits(uint8_t bAlt, uint8_t bPress, uint8_t bTemp)
 		{
@@ -343,7 +343,7 @@ public:
 		{
 			fluidDensity = fDensity;
 		}
-    
+
 private:
 	uint8_t _model; //Sensor model as read from the device
 	bool bInitialized;
@@ -363,14 +363,14 @@ private:
 		uint32_t D1_pres, D2_temp;
 		int32_t TEMP;
 		int32_t P;
-	
-	uint8_t crc4(uint16_t n_prom[]) 
+
+	uint8_t crc4(uint16_t n_prom[])
 	{
     	uint16_t n_rem = 0;
-    
+
     	n_prom[0] = ((n_prom[0]) & 0x0FFF);
     	n_prom[7] = 0;
-    
+
     	for ( uint8_t i = 0 ; i < 16; i++ ) {
     		if ( i%2 == 1 ) {
     			n_rem ^= (uint16_t)((n_prom[i>>1]) & 0x00FF);
@@ -385,21 +385,21 @@ private:
     			}
     		}
     	}
-    
+
     	n_rem = ((n_rem >> 12) & 0x000F);
-    
+
     	return n_rem ^ 0x00;
     }
-    
+
 	//Pressure returned in mbar or mbar*conversion rate.
-		float Pressure(float conversion = 1.0f) 
+		float Pressure(float conversion = 1.0f)
 		{
-			if ( _model == MS5837_VERSION_02BA01 || _model == MS5837_VERSION_02BA21 || _model == MS5837_VERSION_02BA06) 
+			if ( _model == MS5837_VERSION_02BA01 || _model == MS5837_VERSION_02BA21 || _model == MS5837_VERSION_02BA06)
 				return P*conversion/100.0f;
-			else 
+			else
 				return P*conversion/10.0f;
 		}
-    
+
 	//Temperature returned in deg C.
 		float Temperature()
 		{
@@ -407,7 +407,7 @@ private:
 		}
 
 	//Calculate the depth, given the sensed pressure and known ambient pressure
-		float Depth() 
+		float Depth()
 		{
 			// The pressure sensor measures absolute pressure, so it will measure the atmospheric pressure + water pressure
 			// We subtract the atmospheric pressure to calculate the depth with only the water pressure
@@ -415,11 +415,11 @@ private:
 		}
 
 	//Calculate Pressure Altitude in m (relative to sea level at ISA)
-		float Altitude() 
+		float Altitude()
 		{
 			return (1-pow((Pressure()/(atmosphericpress/100)),.190284))*145366.45*.3048;
 		}
-    
+
 	//If an update fails, invalidate all the sensors. ("Unknown" in Home Assistant)
 		void InvalidateSensors()
 		{
@@ -444,7 +444,7 @@ private:
 
 			return fAlt;
 		}
-    
+
 	//Convert the temperature output from °C to the user's chosen units
 		float ConvertTemp(float fTemp)
 		{
